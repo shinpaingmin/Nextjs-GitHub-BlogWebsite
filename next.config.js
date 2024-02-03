42d5b7572f3d2e794bfd4c9b10f4052a910a66c9
shinpaingmin <shinpaingmin@gmail.com>
2024-02-03 22:22:27 +0630

    added config for remote image

--- next.config.js
+++ next.config.js
@@ -3,6 +3,17 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  // for remote source
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: '',
+        pathname: '/shinpaingmin/testing-blogposts/master/images/**'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
