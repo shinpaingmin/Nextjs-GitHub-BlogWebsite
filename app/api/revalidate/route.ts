0e770132aa6a7dcaf272ce29a73ae802022beb5a
shinpaingmin <shinpaingmin@gmail.com>
2024-02-03 22:22:55 +0630

    added on-demand revalidation

---
+++ app/api/revalidate/route.ts
@@ -0,0 +1,24 @@
+// http://localhost:3000/api/revalidate?path=/&secret=HelloWorld
+
+import { NextRequest, NextResponse } from "next/server";
+import { revalidatePath } from 'next/cache';
+
+export async function GET(request: NextRequest) {
+    const secret = request.nextUrl.searchParams.get('secret');
+
+    if(secret !== process.env.MY_SECRET_CODE) {
+        return new NextResponse(JSON.stringify({ message: "Invalid Token" }), {
+            status: 401,
+            statusText: "Unauthorized",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+    }
+
+    const path = request.nextUrl.searchParams.get('path') || '/'
+
+    revalidatePath(path);
+
+    return NextResponse.json({ revalidated: true });
+}
